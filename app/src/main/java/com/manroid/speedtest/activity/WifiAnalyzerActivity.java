d425da1989ec9c64f3f651ed257f535a1f698f17
dinhpv <dinhpv@tohsoft.com>
2021-02-08 09:10:12 +0700

    - update view for wifi analyzer

--- app/src/main/java/com/manroid/speedtest/activity/WifiAnalyzerActivity.java
+++ app/src/main/java/com/manroid/speedtest/activity/WifiAnalyzerActivity.java
@@ -1,17 +1,194 @@
 package com.manroid.speedtest.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import com.manroid.speedtest.R;
+import com.manroid.speedtest.wifianalyzer.ConnectionInfo;
+import com.manroid.speedtest.wifianalyzer.NetworkStatus;
 
-public class WifiAnalyzerActivity extends AppCompatActivity {
+import java.util.Locale;
+
+@SuppressWarnings("deprecation")
+public class WifiAnalyzerActivity extends AppCompatActivity implements ActionBar.TabListener {
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_analyzer);
+
+        // Set up the action bar.
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null)
+        {
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        }
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                if (actionBar != null) {
+                    actionBar.setSelectedNavigationItem(position);
+                }
+            }
+        });
+
+        Drawable drawable[] = new Drawable[mSectionsPagerAdapter.getCount()];
+        drawable[0] = getResources().getDrawable(R.drawable.icon_tab_cinf);
+        drawable[1] = getResources().getDrawable(R.drawable.icon_tab_ns);
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            if (actionBar != null) {
+                actionBar.addTab(
+                        actionBar.newTab()
+                                .setTabListener(this).setIcon(drawable[i]));
+            }
+        }
+
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+        {
+            actionBar.setDisplayOptions( ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE);
+        }
+
+        assert actionBar != null;
+
+        switch (tab.getPosition())
+        {
+            case 0:
+                actionBar.setSubtitle(getString(R.string.title_section_name1)); break;
+            case 1:
+                actionBar.setSubtitle(getString(R.string.title_section_name2)); break;
+
+        }
+
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**  -----------------------------------------------------------------------------------
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     --------------------------------------------------------------------------------------*/
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    // Top Rated fragment activity
+                    return new ConnectionInfo();
+                case 1:
+                    // Games fragment activity
+                    return new NetworkStatus();
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            // Show 5 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+    /**  -------------------------------------------------------------------------------------------
+     * A placeholder fragment containing a simple view.
+     ----------------------------------------------------------------------------------------------*/
+    @SuppressWarnings("unused")
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+
+        @SuppressWarnings("unused")
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
     }
 
 }
+
